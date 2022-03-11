f0d1050bd6b3450374f827337498a536af123fb0
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-03-11 22:39:43 +0530

    minor changes

---
+++ jj.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', ()=> {
+    const form=document.querySelector("#task-form");
+    const input=document.querySelector("#task-input");
+    const list=document.querySelector("#task-list");
+    console.log(form);
+});
