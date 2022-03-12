7fbbf81e0cb57c37085c9f2cc97d85fab81416fb
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-03-12 20:21:01 +0530

    js add

--- jj.js
+++ jj.js
@@ -3,4 +3,14 @@ window.addEventListener('load', ()=> {
     const input=document.querySelector("#task-input");
     const list=document.querySelector("#task-list");
     console.log(form);
+    form.addEventListener('submit' ,(e)=>{
+        e.preventDefault();
+        //preventDefault() is used not refresh the page 
+        console.log("submitted");
+        const task = input.value;
+        if(!task){
+            alert("Please enter a task");
+            return
+        }
+    });
 });
